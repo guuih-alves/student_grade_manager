12841a660b15b913583779ce9384a2ce2122c5f2
guuih_alves <guilherme_alves_08@hotmail.com>
2025-06-19 18:14:19 -0300

    PUT and DELETE

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import http from 'http'
 import {v4} from 'uuid'
 
 const port = 3000;
-const grades = [];
+const grades = [    'oi'];
 
 const server = http.createServer((request, response) => {
     const {method, url} = request;
@@ -13,8 +13,9 @@ const server = http.createServer((request, response) => {
     })
 
     request.on('end', () => {
+        const id = url.split('/')[2];   //referente a segunda parte da url(id)
 
-        if (url === '/grades' && method === 'GET'){
+    if (url === '/grades' && method === 'GET'){
         response.writeHead(200, {'Content-type': 'application/json'});
         response.end(JSON.stringify(grades));
 
@@ -25,9 +26,37 @@ const server = http.createServer((request, response) => {
         response.writeHead(201, {'Content-type': 'application/json'});
         response.end(JSON.stringify(newGrade));
     } 
+
+    else if (url.startsWith ('/grades/') && method === 'PUT'){
+        const { studentName, subject, grade} = JSON.parse(body);
+        const gradesToUpdate = grades.find(g => g.id === id );
+        if (gradesToUpdate){
+            gradesToUpdate.studentName =studentName;
+            gradesToUpdate.subject = subject;
+            gradesToUpdate.grade = grade;
+            response.writeHead(200, {'Content-type': 'application/json'});
+            response.end(JSON.stringify(gradesToUpdate));
+        }
+        else {
+        response.writeHead(404, {'Content-type': 'application/json'});
+        response.end(JSON.stringify({ message: 'Grade not found'}));
+       }
+    }
+
+    else if (url.startsWith ('/grades/') && method === 'DELETE'){
+        const index = grades.findIndex(g => g.id === id);
+        if(index !== -1) {  // -1 é porque o item nao existe
+            grades.splice(index, 1);   
+            response.writeHead(204);
+            response.end;
+        } else{
+          response.writeHead(404, {'Content-type': 'application/json'});
+         response.end(JSON.stringify({ message: 'Grade not found'}));  
+        }
+    }
     
     else {
-        response.writeHead(400, {'Content-type': 'application/json'});
+        response.writeHead(404, {'Content-type': 'application/json'});
         response.end(JSON.stringify({ message: 'Route not found'}));
     }
     });
